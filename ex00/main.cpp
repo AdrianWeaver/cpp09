8848c0c3ab907332a89e05b70e09d373e37dbc9e
Adrian WEAVER <aweaver@student.42.fr>
2023-05-12 19:26:21 +0200

    ADD DATE COMPARISON (ex00):
    Added a straightforward date comparison that outputs the value of
    bitcoin at this date and explicits when the value is not found.
    
    This is not at all what was asked by the subject, still a fun feature.

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -2,13 +2,14 @@
 #include <fstream>
 #include <map>
 
+#define DATABASE_FILE "data.csv"
 //int isValidDate(std::fstream RawKey)
 //{
 
 //}
-void	storeDatabase(char *fileName, std::map<std::string, std::string> &bitcoinDatabase)
+void	storeDatabase(std::map<std::string, std::string> &bitcoinDatabase)
 {
-	std::ifstream inputFile(fileName);
+	std::ifstream inputFile(DATABASE_FILE);
 	if (inputFile)
 	{
 		char buffer1[256];
@@ -19,13 +20,34 @@ void	storeDatabase(char *fileName, std::map<std::string, std::string> &bitcoinDa
 			inputFile.getline(buffer2, 256);
 			bitcoinDatabase.insert(std::make_pair(buffer1, buffer2));
 		}
-		for (std::map<std::string, std::string>::iterator it = bitcoinDatabase.begin(); it != bitcoinDatabase.end(); it++)
-			std::cout << it->first << " = " << it->second << std::endl;
 	}
 	else
 	{
 		std::cout << "Invalid database file" << std::endl;
 	}
+}
+
+void	compareWithFile(char *fileName, std::map<std::string, std::string> &bitcoinDatabase)
+{
+	std::ifstream compareFile(fileName);
+	if (compareFile)
+	{
+		char buffer[256];
+		while (compareFile.getline(buffer, 256, '\n'))
+		{
+			std::map<std::string, std::string>::iterator found;
+			found = bitcoinDatabase.find(buffer);
+			if (found != bitcoinDatabase.end())
+				std::cout << "bitcoin value at " << buffer << " was : " << found->second << std::endl;
+			else
+			{
+				std::cerr << "No correspondance in database for \"" << buffer << "\" please check date format YYYY-MM-DD" <<
+					std::endl << "valid dates between 2009-01-02 and 2022-03-39" << std::endl;
+			}
+		}
+	}
+	else
+		std::cout << "Invalid comparision file, aborting." << std::endl;
 }
 
 int	main(int argc, char **argv)
@@ -37,6 +59,7 @@ int	main(int argc, char **argv)
 		std::cout << "Please provide database file" << std::endl;
 		return (0);
 	}
-	storeDatabase(argv[1], bitcoinDatabase);
+	storeDatabase(bitcoinDatabase);
+	compareWithFile(argv[1], bitcoinDatabase);
 	return (0);
 }
